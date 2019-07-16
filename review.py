f0f5c06aa8c31e9b28619d53debbfaaa3a3b95e1
Sun0506 <asd8061812200@gmail.com>
2019-07-16 23:45:30 +0800

    calc len & average

--- review.py
+++ review.py
@@ -4,7 +4,13 @@ data = []
 with open('reviews.txt', 'r') as file:
     for message in file:
         data.append(message)  # append 新增 message 至 data 清單裡
-print(len(data))    # len 字串的長度
-print(data[2])
-print('-----------------------------')
-print(data[4])
+#       print(len(data))    # 會一筆一筆列出
+print(len(data))    # len 字串的長度  總直接列出筆數
+# print(data[2])
+# print('-----------------------------')
+# print(data[4])
+
+sum_len = 0
+for m in data:
+	sum_len = sum_len + len(m)
+print('留言的平均長度為', sum_len/len(data))
